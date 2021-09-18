bba155c278ccbe8198f09586f00e9d35f311f80f
Addison <44730071+AddisonAV@users.noreply.github.com>
2021-09-17 22:30:46 -0300

    Added an global game instance and sound manager
    I don't care...

---
+++ TheGame2/Assets/Scripts/GameAsset.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameAsset : MonoBehaviour
+{
+    public static GameAsset Instance { get; private set; }
+    //public Player player; config the player variable after
+
+    public PSoundAudioClip[] playerAudioClipsArray;
+    public SoundAudioClip[] soundAudioClipsArray;
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public SoundManager.Sound sound;
+        public AudioClip audioClip;
+    }
+    
+    [System.Serializable]
+    public class PSoundAudioClip
+    {
+        public SoundManager.PlayerSound sound;
+        public AudioClip audioClip;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ TheGame2/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+public class SoundManager : MonoBehaviour
+{
+    public enum PlayerSound
+    {
+        weaponAKFire,
+        weaponGunFire,
+        PlayerHurt,
+        PlayerDie
+    }
+
+    public enum Sound
+    {
+        DoorOpen,
+    }
+
+    private static GameObject oneShotGameObject;
+    private static AudioSource oneShotAudioSource;
+
+    public static void PlaySound(Sound sound, float volume, Vector3 position)
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        soundGameObject.transform.position = position;
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.clip = GetAudioClip(sound);
+        audioSource.maxDistance = 100f;
+        audioSource.spatialBlend = 1f;
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+        audioSource.dopplerLevel = 0f;
+        audioSource.volume = volume;
+        audioSource.Play();
+        Object.Destroy(soundGameObject, audioSource.clip.length);
+    }
+    public static void PlaySound(Sound sound, float volume)
+    {
+        if (!oneShotGameObject)
+        {
+            oneShotGameObject = new GameObject("One Shot Sound");
+            oneShotAudioSource = oneShotGameObject.AddComponent<AudioSource>();
+        }
+        oneShotAudioSource.PlayOneShot(GetAudioClip(sound), volume);
+    }
+
+    private static AudioClip GetAudioClip(PlayerSound sound)
+    {
+        foreach (var variable in GameAsset.Instance.playerAudioClipsArray)
+        {
+            if (variable.sound == sound)
+            {
+                return variable.audioClip;
+            }
+        }
+        Debug.Log("Sound: "+ sound + " not found!");
+        return null;
+    }
+    
+    private static AudioClip GetAudioClip(Sound sound)
+    {
+        foreach (var variable in GameAsset.Instance.soundAudioClipsArray)
+        {
+            if (variable.sound == sound)
+            {
+                return variable.audioClip;
+            }
+        }
+        Debug.Log("Sound: "+ sound + " not found!");
+        return null;
+    }
+}
